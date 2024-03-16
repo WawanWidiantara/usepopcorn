bb131340a604892d6449f115a5b73547dbff1354
WawanWidiantara <gdwidi13@gmail.com>
2024-03-16 22:02:18 +0700

    hover rating effect

--- src/StarRating.js
+++ src/StarRating.js
@@ -17,9 +17,18 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
+  }
+
+  function handleHoverIn(rating) {
+    setTempRating(rating);
+  }
+
+  function handleHoverOut() {
+    setTempRating(0);
   }
 
   return (
@@ -29,11 +38,13 @@ export default function StarRating({ maxRating = 5 }) {
           <Star
             key={i}
             onRate={() => handleRating(i + 1)}
-            full={rating >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => handleHoverIn(i + 1)}
+            onHoverOut={() => handleHoverOut()}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
@@ -45,9 +56,14 @@ const starStyle = {
   cursor: "pointer",
 };
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span style={starStyle} onClick={onRate}>
+    <span
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
