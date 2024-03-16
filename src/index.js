cb96faf05737ceb906a1dc8d538b2f0552ba8f32
WawanWidiantara <gdwidi13@gmail.com>
2024-03-16 22:29:09 +0700

    reusability props star rating

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import StarRating from "./StarRating";
+import { useState } from "react";
+
+function Test() {
+  const [movieRating, setRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        className={"test"}
+        message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+        onSetRating={setRating}
+      />
+      <p>This movie has {movieRating} rating</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <Test />
   </React.StrictMode>
 );
